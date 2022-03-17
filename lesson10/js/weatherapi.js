6cf934666bd5f332ec370a433b959d76fe52877f
arthureric <art21005@byui.edu>
2022-03-17 09:11:25 +0000

    commit

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,5 +1,5 @@
 const apiURL =
-  "https://api.openweathermap.org/data/2.5/weather?id=5861897&units=imperial&APPID=f3c723af6991221a76f6d1976bbfd2c7";
+  "https://api.openweathermap.org/data/2.5/weather?id=5596467&units=imperial&APPID=f3c723af6991221a76f6d1976bbfd2c7";
 
 fetch(apiURL)
   .then((response) => response.json())
